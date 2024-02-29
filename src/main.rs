09440efe51f529f1867dd204c37bd13b2bbd37f8
Kenneth Lo <closer.tw@gmail.com>
2024-02-29 20:03:03 +0800

    [feat] Loads image from file

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-extern crate sdl2;
-
 use sdl2::event::Event;
+use sdl2::image::{self, LoadTexture, InitFlag};
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
+use sdl2::render::{WindowCanvas, Texture};
 use std::time::Duration;
 
-use rand::prelude::*;
+const SCREEN_W: u32 = 800;
+const SCREEN_H: u32 = 600;
+const GRID_W: u32 = 40;
+const GRID_H: u32 = 40;
+const GRID_COUNT_X: usize = (SCREEN_W / GRID_W) as usize;
+const GRID_COUNT_Y: usize = (SCREEN_H / GRID_H) as usize;
 
 pub fn main() -> Result<(), String> {
-    const SCREEN_W: u32 = 800;
-    const SCREEN_H: u32 = 600;
-    const GRID_W: u32 = 40;
-    const GRID_H: u32 = 40;
-    const GRID_COUNT_X: usize = (SCREEN_W / GRID_W) as usize;
-    const GRID_COUNT_Y: usize = (SCREEN_H / GRID_H) as usize;
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    let _image_context = image::init(InitFlag::JPG | InitFlag::PNG)?;
 
     let window = video_subsystem
         .window("rust-sdl2 demo: Video", SCREEN_W, SCREEN_H)
@@ -28,28 +28,22 @@ pub fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump()?;
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
 
-    let mut rng = thread_rng();
     let delta_up = 0.1;
     let delta_down = -0.03;
 
     let mut levels = (0..GRID_COUNT_Y)
-        .map(|_| (0..GRID_COUNT_X).map(|_| rng.gen()).collect::<Vec<f64>>())
+        .map(|_| (0..GRID_COUNT_X).map(|_| 0.0).collect::<Vec<f64>>())
         .collect::<Vec<_>>();
 
-    let mut deltas = (0..GRID_COUNT_Y)
-        .map(|_y| {
-            (0..GRID_COUNT_X)
-                .map(|_x| if rng.gen() { delta_up } else { delta_down })
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
+    let mut cur_x = -1;
+    let mut cur_y = -1;
 
+    let mut event_pump = sdl_context.event_pump()?;
     'running: loop {
+        // Handle input
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -57,39 +51,63 @@ pub fn main() -> Result<(), String> {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::MouseMotion { x, y, .. } => {
+                    cur_x = x;
+                    cur_y = y;
+                }
                 _ => {}
             }
         }
 
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-
-        // update levels
+        // Update levels
+        let in_y = if cur_y >= 0 { cur_y / 40 } else { -1 };
+        let in_x = if cur_x >= 0 { cur_x / 40 } else { -1 };
         for y in 0..GRID_COUNT_Y {
             for x in 0..GRID_COUNT_X {
-                levels[y][x] += deltas[y][x];
-                if levels[y][x] > 1.0 {
-                    levels[y][x] = 1.0;
-                    deltas[y][x] = delta_down;
-                } else if levels[y][x] < 0.0 {
-                    levels[y][x] = 0.0;
-                    deltas[y][x] = delta_up;
+                if (y as i32) == in_y && (x as i32) == in_x {
+                    levels[y][x] += delta_up;
+                    if levels[y][x] > 1.0 {
+                        levels[y][x] = 1.0;
+                    }
+                } else {
+                    levels[y][x] += delta_down;
+                    if levels[y][x] < 0.0 {
+                        levels[y][x] = 0.0;
+                    }
                 }
-                let red = (255.0 * levels[y][x]) as u8;
-                canvas.set_draw_color(Color::RGB(red, 0, 0));
-                canvas.draw_rect(Rect::new(
-                    (x as u32 * GRID_W + 1) as i32,
-                    (y as u32 * GRID_H + 1) as i32,
-                    (GRID_W - 2) as u32,
-                    (GRID_H - 2) as u32,
-                ))?;
             }
         }
 
-        canvas.present();
+        // Render
+        render(&mut canvas, &levels, &texture)?;
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
         // The rest of the game loop goes here...
     }
+
+    Ok(())
+}
+
+fn render(canvas: &mut WindowCanvas, levels: &Vec<Vec<f64>>, texture: &Texture) -> Result<(), String> {
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    for y in 0..GRID_COUNT_Y {
+        for x in 0..GRID_COUNT_X {
+            let red = (255.0 * levels[y][x]) as u8;
+            canvas.set_draw_color(Color::RGB(red, 0, 0));
+            canvas.fill_rect(Rect::new(
+                (x as u32 * GRID_W + 1) as i32,
+                (y as u32 * GRID_H + 1) as i32,
+                (GRID_W - 2) as u32,
+                (GRID_H - 2) as u32,
+            ))?;
+        }
+    }
+
+    canvas.copy(texture, None, None)?;
+
+    canvas.present();
 
     Ok(())
 }

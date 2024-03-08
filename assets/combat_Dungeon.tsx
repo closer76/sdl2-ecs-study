c37e4c4eaaf2b15dd460d0d96803f9fb3747d1ba
Kenneth Lo <closer.tw@gmail.com>
2024-03-08 09:21:08 +0800

    [feat] Adds Tiled created project files

---
+++ assets/combat_Dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Combat Dungeon" tilewidth="48" tileheight="48" spacing="12" margin="2" tilecount="30" columns="10">
+ <image source="combat_Dungeon.png" width="608" height="176"/>
+</tileset>
---
+++ assets/darkdimension.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Dark Dimension" tilewidth="16" tileheight="16" tilecount="609" columns="29">
+ <image source="darkdimension.png" width="464" height="336"/>
+</tileset>
